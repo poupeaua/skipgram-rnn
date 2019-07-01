d88aa6ac2b90ef735bb38af7d62b623846bc3c83
Matías Pizarro <matias.pizarro.o96@gmail.com>
2019-07-01 18:02:30 -0400

    got results

--- tools/get_results.py
+++ tools/get_results.py
@@ -28,12 +28,95 @@ def read_results(model_path, model_name=''):
                 res['metrics'] = metrics
     return res
 
+
 def get_results():
     models_res = {}
     for md in MODELS_DIRS:
-        print(md)
         curr_res = read_results(model_path=os.path.join(MODEL_PATH, md), model_name=md)
         if curr_res['model_name'] not in models_res:
             models_res[curr_res['model_name']] = curr_res['metrics']
     return models_res
 
+
+# Function that allows to plot confusion matrix
+
+# IMPORTANT: this function has been extracted from open source code in the internet, and modified to receive the correct
+# parameters.
+# reference: https://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+
+def plot_confusion_matrix(matrix, classes_list, normalize=False, title=None, cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+
+    # Compute confusion matrix
+    cm = matrix
+    # Only use the labels that appear in the data
+    # classes_list = classes_list[unique_labels(y_true, y_pred)]
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes_list, yticklabels=classes_list,
+           title=title,
+           ylabel='Etiqueta Correcta',
+           xlabel='Etiqueta Predicha')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    return ax
+
+
+results = get_results()
+
+windows = [2, 3, 5, 10]
+sizes = [10, 25, 50, 100, 200, 500]
+
+best_cm = results['model_500_10']['cm']
+
+plot_confusion_matrix(best_cm, ['Mala', 'Buena'], normalize=True, title='Matriz de Confusión para Modelo M500-10')
+plt.savefig('cm.png')
+plt.show()
+
+# for w in windows:
+#     acc = []
+#     for s in sizes:
+#         model = 'model_{}_{}'.format(s, w)
+#         acc.append(results[model]['accuracy'])
+#     plt.plot(sizes, acc, label='window size = {}'.format(w), marker='.', linewidth='1')
+#
+# plt.xlabel('Tamaño de los Vectores de Palabras')
+# plt.ylabel('Precisión')
+# plt.title('Gráfico de Presición v/s Tamaño de Vector\npara Distintos Tamaños de Ventanas')
+# plt.grid(True)
+# plt.legend()
+# plt.savefig('plot.png')
+# plt.show()
